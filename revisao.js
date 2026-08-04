157953892894ab781537b929e016ab83eb1ea641
YASMIN SAYURI SAITO MENA <26171683@edu119.local>
2026-08-04 09:53:19 -0300

    Salvando Alterações

--- revisao.js
+++ revisao.js
@@ -1,1 +1,23 @@
 console.log("Olá mundo");
+
+let idade = 20;
+
+console.log(idade);
+
+let numero = 10;
+
+console.log(numero + 5);  // Operador - Adição
+console.log(numero - 5);  // Operador - Subtração
+console.log(numero * 5);  // Operador - Multiplicação
+console.log(numero / 5);  // Operador - Divisão
+console.log(numero % 5);  // Operador - Módulo
+
+//### Operadores
+// > Maior que
+// < Menor que
+// >= Maior ou igual
+// <= Menor ou igual
+// == Igual (valor)
+// === Estritamente
+// != Diferente
+// !== Estritamente Diferente
