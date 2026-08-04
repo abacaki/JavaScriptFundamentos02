cc12ea19a327b8060dcce22c322fdff55785f4aa
YASMIN SAYURI SAITO MENA <26171683@edu119.local>
2026-08-04 14:09:12 -0300

    Salvando Alterações 3

--- lacos.js
+++ lacos.js
@@ -1,3 +1,69 @@
-for (let i = 1; i <= 5; i++) {
-    console.log(i);
+// for (let i = 1; i <= 5; i++) {
+//         console.log(i);
+// }
+
+// for (let i = 10; i >= 1; i--){
+//     console.log(i);
+// }
+
+// Números pares
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// Números impares
+// for (let i = 1; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+//Tabuada
+// for(let i = 1; i <= 10; i ++) {
+//     console.log(`5 X ${i} = ${5 * i}`);
+// }
+
+//Minuto
+// for(let minuto = 0; minuto <=59; minuto ++) {
+//     console.log(`Minuto: ${minuto}`);
+// }
+
+//Hora
+// for(let hora = 0; hora <=23; hora ++) {
+//     console.log(`Hora: ${hora}`);
+// }
+
+
+// for(let hora = 0; hora <=23; hora ++) {
+//     for(let minuto = 0; minuto <=59; minuto ++) {
+//     console.log(`${hora}:${minuto}`);
+//     }
+// }
+
+// Pulando o número
+// for(let i = 1; i <= 10; i++) {
+//     if(i == 5) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// Para o laço
+// for(let i = 1; i <= 10; i++) {
+//     if(i == 5) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// Soma
+// let soma = 0;
+// for (let i = 1; i <= 10; i++ ) {
+//     soma += i;
+// }
+// console.log(`Soma final é: ${soma}`);
+
+let numero = 1;
+
+while(numero <= 10) {
+    console.log(numero);
+    numero++;
 }
